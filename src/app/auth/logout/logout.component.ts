a6f5b7db89686b982593c5dc3c5de86a984f53cb
maraun <mukhtargaliyev.rauan@gmail.com>
2019-12-27 15:32:15 +0600

    Logout works!

--- src/app/auth/logout/logout.component.ts
+++ src/app/auth/logout/logout.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {TokenStorageService} from '../token-storage.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'ngx-logout',
   templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.scss']
+  styleUrls: ['./logout.component.scss'],
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private  tokenStorage: TokenStorageService, private router: Router) { }
 
   ngOnInit() {
+    this.tokenStorage.signOut();
+    this.router.navigateByUrl('/auth/login');
   }
 
 }
