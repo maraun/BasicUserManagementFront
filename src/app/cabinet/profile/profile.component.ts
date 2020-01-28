43c54e54289370da041965c9ea537f66d69ec605
maraun <mukhtargaliyev.rauan@gmail.com>
2020-01-28 13:23:54 +0600

    Finished Profile page

--- src/app/cabinet/profile/profile.component.ts
+++ src/app/cabinet/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../../models/user';
-import {UserService} from '../../services/user.service';
+import {User} from '../../@core/models/profile/user';
+import {UserService} from '../../@core/services/user.service';
 import {NbIconConfig, NbToastrService} from '@nebular/theme';
 
 @Component({
