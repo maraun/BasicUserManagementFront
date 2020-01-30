067fe3b370130b8fdeae2a1061bc13ec697f243d
maraun <mukhtargaliyev.rauan@gmail.com>
2020-01-30 09:43:12 +0600

    Toaster embed

--- src/app/cabinet/users/list/modal/modal.component.ts
+++ src/app/cabinet/users/list/modal/modal.component.ts
@@ -3,6 +3,7 @@ import {NbDialogRef, NbIconConfig, NbToastrService} from '@nebular/theme';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../../../@core/models/profile/user';
 import {UserService} from '../../../../@core/services/user.service';
+import {ToastService} from '../../../../@core/services/toast.service';
 
 @Component({
   selector: 'ngx-modal',
@@ -19,8 +20,10 @@ export class ModalComponent implements OnInit {
   loading = false;
   userExist = false;
 
-  constructor(protected ref: NbDialogRef<ModalComponent>, private fb: FormBuilder,
-              private toastrService: NbToastrService, private userService: UserService) {
+  constructor(protected ref: NbDialogRef<ModalComponent>,
+              private fb: FormBuilder,
+              private toast: ToastService,
+              private userService: UserService) {
   }
 
   dismiss() {
@@ -30,18 +33,65 @@ export class ModalComponent implements OnInit {
   ngOnInit() {
     this.initForm();
     this.loading = true;
+    this.loadData();
+  }
+
+  onFirstSubmit() {
+    this.firstForm.markAsDirty();
+  }
+
+  onSecondSubmit() {
+    this.secondForm.markAsDirty();
+  }
+
+  onThirdSubmit() {
+    this.thirdForm.markAsDirty();
+    if (this.id === 0) {
+      this.toast.success('User created', 'person-done-outline');
+    } else {
+      this.toast.success('Changes saved', 'cloud-upload-outline');
+    }
+  }
+  initForm() {
+    this.firstForm = new FormGroup({
+      iin: new FormControl(),
+      password: new FormControl(),
+      lastname: new FormControl(),
+      firstname: new FormControl(),
+      middlename: new FormControl(),
+      previousLastname: new FormControl(),
+      birthDate: new FormControl(),
+      gender: new FormControl(),
+      maritalStatus: new FormControl(),
+    });
+    this.secondForm = new FormGroup({
+      nationality: new FormControl(),
+      citizenship: new FormControl(),
+      livingPlace: new FormControl(),
+      registrationPlace: new FormControl(),
+      email: new FormControl(),
+      mobilePhone: new FormControl(),
+      workPhone: new FormControl(),
+      homePhone: new FormControl(),
+    });
+    this.thirdForm = new FormGroup({
+      additional: new FormControl(),
+    });
+  }
+
+  loadData() {
     if (this.id === 0) {
       this.user = new User();
     } else {
       this.userService.getByProfileId(this.id).subscribe((perf) => {
           this.user = perf;
           this.loading = false;
-          this.userLoaded();
+          this.userExist = !!this.user;
           this.firstForm = this.fb.group({
             iin: [this.user.username, Validators.required],
             password: ['*', Validators.required],
             lastname: [this.user.profile.lastname, Validators.required],
-            name: [this.user.profile.firstname, Validators.required],
+            firstname: [this.user.profile.firstname, Validators.required],
             middlename: [this.user.profile.middlename],
             previousLastname: [this.user.profile.previouslastname],
             birthDate: [this.user.profile.birthdate, Validators.required],
@@ -64,76 +114,8 @@ export class ModalComponent implements OnInit {
           });
         },
         err => {
-          this.showErrorToast();
+          this.toast.error('Data not load', 'cloud-download-outline');
         });
-    }
-  }
-
-  onFirstSubmit() {
-    this.firstForm.markAsDirty();
-  }
-
-  onSecondSubmit() {
-    this.secondForm.markAsDirty();
-  }
-
-  onThirdSubmit() {
-    this.thirdForm.markAsDirty();
-    this.user.profile.iin = this.firstForm.get('iin').value;
-    if (this.id === 0) {
-      this.showCreateToast();
-    } else {
-      this.showUpdateToast();
-    }
-  }
-
-  showErrorToast() {
-    const iconConfig: NbIconConfig = {icon: 'cloud-download-outline', pack: 'eva', status: 'danger'};
-    this.toastrService.show('Data loading error', `Error`, iconConfig);
-  }
-
-  showUpdateToast() {
-    const iconConfig: NbIconConfig = {icon: 'cloud-upload-outline', pack: 'eva', status: 'success'};
-    this.toastrService.show('Changes saved', `Done`, iconConfig);
-  }
-
-  showCreateToast() {
-    const iconConfig: NbIconConfig = {icon: 'cloud-upload-outline', pack: 'eva', status: 'success'};
-    this.toastrService.show('User created', `Done`, iconConfig);
-  }
-
-  initForm() {
-    this.firstForm = new FormGroup({
-      iin: new FormControl(),
-      password: new FormControl(),
-      lastname: new FormControl(),
-      name: new FormControl(),
-      middlename: new FormControl(),
-      previousLastname: new FormControl(),
-      birthDate: new FormControl(),
-      gender: new FormControl(),
-      maritalStatus: new FormControl(),
-    });
-    this.secondForm = new FormGroup({
-      nationality: new FormControl(),
-      citizenship: new FormControl(),
-      livingPlace: new FormControl(),
-      registrationPlace: new FormControl(),
-      email: new FormControl(),
-      mobilePhone: new FormControl(),
-      workPhone: new FormControl(),
-      homePhone: new FormControl(),
-    });
-    this.thirdForm = new FormGroup({
-      additional: new FormControl(),
-    });
-  }
-
-  userLoaded() {
-    if (!!this.user) {
-      this.userExist = true;
-    } else {
-      this.userExist = false;
     }
   }
 }
