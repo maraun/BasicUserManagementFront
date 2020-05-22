b263cf03233732946a69a8cf735a6c436a98bf89
maraun <mukhtargaliyev.rauan@gmail.com>
2020-05-22 16:49:24 +0600

    Menu item hide init

--- src/app/cabinet/cabinet.component.ts
+++ src/app/cabinet/cabinet.component.ts
@@ -1,13 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import {CMENU_ITEMS} from './cabinet-menu';
+import {NbMenuItem} from '@nebular/theme';
 
 @Component({
   selector: 'ngx-cabinet',
   templateUrl: './cabinet.component.html',
   styleUrls: ['./cabinet.component.scss'],
 })
+
 export class CabinetComponent {
 
-cmenu = CMENU_ITEMS;
+  CMENU_ITEMS: NbMenuItem[] = [
+    {
+      title: 'Users',
+      icon: 'layout-outline',
+      children: [
+        {
+          title: 'List',
+          link: '/cabinet/users/list',
+          hidden: false,
+        },
+        {
+          title: 'Roles',
+          link: '/cabinet/users/roles',
+          hidden: false,
+        },
+      ],
+    },
+  ]
+cmenu = this.CMENU_ITEMS;
 
 }
+
