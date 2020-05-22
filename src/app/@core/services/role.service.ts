8ee5f8ab80620296baf921e7abd18e5266c59570
maraun <mukhtargaliyev.rauan@gmail.com>
2020-05-22 18:16:27 +0600

    Added hasRole function

--- src/app/@core/services/role.service.ts
+++ src/app/@core/services/role.service.ts
@@ -3,18 +3,29 @@ import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {Observable} from 'rxjs';
 import {Role} from '../models/profile/role';
+import {TokenStorageService} from '../../auth/token-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RoleService {
   apiUrl = environment.apiUrl + '/api/profile/roles';
-  constructor(public http: HttpClient) { }
+  roles: string[] = [];
+  constructor(public http: HttpClient,
+              public tokenStorage: TokenStorageService) { }
   public findAll(): Observable<Role[]> {
     return this.http.get<Role[]>(this.apiUrl);
   }
   public getByRoleId(id): Observable<Role> {
     return this.http.get<Role>(`${this.apiUrl}/${id}`);
+  }
+  public hasRole (checkRole): boolean {
+    this.roles = this.tokenStorage.getAuthorities();
+    if (this.roles.includes(checkRole)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
